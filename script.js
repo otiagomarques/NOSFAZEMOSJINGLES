9698ada1d6013938d0a01c738e5817f435290a89
otiagomarques <80545823+otiagomarques@users.noreply.github.com>
2023-01-10 20:02:04 +0000

    working less js more css

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ const colorSchemes = [
     { color1: "#ff8a00", color2: "#520076"},
     { color1: "#99ffba", color2: "#0079ff"},
     { color1: "#40b37c", color2: "#d40000"},
-    { color1: "ffbdff", color2: "#a4c724"},
     { color1: "#d3ffff", color2: "#ff9500"},
     { color1: "#bfffaa", color2: "#ff00f5"},
     { color1: "#feff00", color2: "#0700c7"},
-    { color1: "#83bc7e", color2: "e44545"},
     { color1: "#b2f4b8", color2: "#242331"},
     { color1: "#f2ff00", color2: "#34c992"}
   ];
@@ -18,47 +16,8 @@ const colorSchemes = [
   window.onload = function() {
     // Choose a random color scheme from the array
    const colorScheme = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
-  
-    // Use the properties of the chosen color scheme to style the page
-    document.body.style.color = colorScheme.color1;
-    document.body.style.backgroundColor = colorScheme.color2;
-
-    let logos = document.getElementById("logos").getElementsByClassName("svg");
-    for (let i = 0; i < logos.length; i++) {
-      logos[i].style.fill = colorScheme.color1;
-    }
-  
-    let modals = document.getElementsByClassName("modal");
-    for (let i = 0; i < modals.length; i++) {
-      modals[i].style.color = colorScheme.color2;
-    }
-  
-    let contents = document.getElementsByClassName("modal-content");
-    for (let i = 0; i < contents.length; i++) {
-      contents[i].style.backgroundColor = colorScheme.color1;
-    }
-  
-    let mlogos = document.getElementsByClassName("modal-logo");
-    for (let i = 0; i < mlogos.length; i++) {
-      mlogos[i].style.fill = colorScheme.color2;
-    }
-  
-    let shadows = document.getElementsByClassName("svg");
-    for (let i = 0; i < shadows.length; i++) {
-      shadows[i].addEventListener("mouseover", function() {
-        shadows[i].style.filter = `drop-shadow(1px 0 15px ${colorScheme.color1})`;
-        shadows[i].style.fill = "white";
-      });
-      shadows[i].addEventListener("mouseout", function() {
-        shadows[i].style.filter = "none";
-        shadows[i].style.fill = colorScheme.color1;
-      });
-    }
-
-    let audios = document.getElementsByClassName("audio");
-    for (let i = 0; i < audios.length; i++) {
-    audios[i].style.backgroundColor = colorScheme.color2;
-    }
+    document.documentElement.style.setProperty('--color1', colorScheme.color1);
+    document.documentElement.style.setProperty('--color2', colorScheme.color2);
   }
 
 //Modal Action
